9ab8ff2a98da336c06aff43fab9fd3a7c8f4fa97
wh00hw <00wh00@protonmail.com>
2022-07-24 16:43:40 +0200

    added music_player timeout

--- lib/music_player.py
+++ lib/music_player.py
@@ -1,17 +1,23 @@
-import threading
+from threading import Thread
+import time
 
 class MusicPlayer:
     def __init__(self, serial_wrapper) -> None:
         self._serial_wrapper = serial_wrapper
         self.thread = None
-    
-    def _run(self, rtttl_code):
-        self._serial_wrapper.send(f"music_player {rtttl_code}")
 
-    def play(self, rtttl_code):
-        self.thread = threading.Thread(target=self._run, args=(rtttl_code,))
+    def play(self, rtttl_code, timeout=10):
+        def _run():
+            self._serial_wrapper.send(f"music_player {rtttl_code}")
+        def _timer(timeout):
+            if self.thread.is_alive():
+                time.sleep(timeout)
+                self.stop()
+        self.thread = Thread(target=_run)
         self.thread.start()
-    
+        if timeout:
+            Thread(target=_timer, args=(timeout,)).start()
+
     def stop(self):
         if self.thread.is_alive():
             self._serial_wrapper.ctrl_c()
