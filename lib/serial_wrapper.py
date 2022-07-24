947871566f5c1d753cffd3337b433db9fc01b45f
wh00hw <00wh00@protonmail.com>
2022-07-24 13:24:09 +0200

    added error handler

--- lib/serial_wrapper.py
+++ lib/serial_wrapper.py
@@ -1,18 +1,30 @@
 import time
 import serial
+import re
+
+def error_handler(func):
+    def handler(*args):
+        result = func(*args)
+        pattern = re.compile("\w+\serror:\s.*")
+        match = pattern.match(result)
+        if match:
+            raise Exception(match.group())
+        return result        
+    return handler
 
 class SerialWrapper:
     def __init__(self, port) -> None:
-        self.serial_port = serial.Serial(port=port, baudrate=9600,
+        self._serial_port = serial.Serial(port=port, baudrate=9600,
                                          bytesize=8, timeout=None, stopbits=serial.STOPBITS_ONE)
-        self.serial_port.read_until(b'>')
+        self._serial_port.read_until(b'>')
 
+    @error_handler
     def send(self, payload):
-        self.serial_port.read()
-        self.serial_port.write(f"{payload}\r".encode())
+        self._serial_port.read()
+        self._serial_port.write(f"{payload}\r".encode())
         time.sleep(0.5)
-        self.serial_port.readline()
-        return self.serial_port.read_until(b'>').decode().rstrip('>\r\n')
+        self._serial_port.readline()
+        return self._serial_port.read_until(b'>').decode().rstrip('>\r\n')
     
     def ctrl_c(self):
-        self.serial_port.write(b'\x03')
+        self._serial_port.write(b'\x03')
