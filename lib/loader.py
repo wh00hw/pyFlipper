82352b7e999692d711ded080c87fc0ff3e1d7797
4n4nk3 <4n4nk3@users.noreply.github>
2022-07-25 13:48:56 +0200

    Small bugfix and code refactor of Loader class.
    - Added checks to prevent empty strings to be saved as application names or category names.
    - Added type hinting.

--- lib/loader.py
+++ lib/loader.py
@@ -1,18 +1,24 @@
+from lib.serial_wrapper import SerialWrapper
+
 class Loader:
-    def __init__(self, serial_wrapper) -> None:
+    def __init__(self, serial_wrapper: SerialWrapper) -> None:
         self._serial_wrapper = serial_wrapper
     
-    def list(self):
+    def list(self) -> dict:
         response = self._serial_wrapper.send("loader list")
         result = {}
         current_category = ""
-        for e in response.split('\n'):
-            if ':' in e:
-                current_category = e[:-2]
+
+        for line in response.split('\n'):
+            if ':' in line:
+                current_category = line[:-2]
                 result[current_category] = []
-                continue
-            result[current_category].append(e.strip())
-        return result
+            else:
+                value = line.strip()
+                if value:
+                    result[current_category].append(value)
+                    
+        return {x: y for x, y in result.items() if x or y}
     
-    def open(self, app_name):
+    def open(self, app_name: str) -> str:
         return self._serial_wrapper.send(f"loader open {app_name}")
