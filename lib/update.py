0bea1e9859b99c3a67e1cafadbf638c3387c6600
4n4nk3 <4n4nk3@users.noreply.github>
2022-07-25 15:01:16 +0200

    Type hinting and asserts implementation

--- lib/update.py
+++ lib/update.py
@@ -1,12 +1,17 @@
+from .serial_wrapper import SerialWrapper
+
 class Update:
-    def __init__(self, serial_wrapper) -> None:
+    def __init__(self, serial_wrapper: SerialWrapper) -> None:
         self._serial_wrapper = serial_wrapper
     
-    def install(self, fuf_file):
+    def install(self, fuf_file: str) -> str:
+        assert fuf_file.endswith('.fuf')
         return self._serial_wrapper.send(f"update install {fuf_file}")
     
-    def backup(self, destination_tar_file):
+    def backup(self, destination_tar_file: str) -> str:
+        assert destination_tar_file.endswith('.tar')
         return self._serial_wrapper.send(f"update backup {destination_tar_file}")
     
-    def restore(self, restore_tar_file):
+    def restore(self, restore_tar_file: str) -> str:
+        assert restore_tar_file.endswith('.tar')
         return self._serial_wrapper.send(f"update restore {restore_tar_file}")
