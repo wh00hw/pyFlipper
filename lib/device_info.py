d15756316a03028da730544347cabbc475cb901e
4n4nk3 <4n4nk3@users.noreply.github>
2022-07-25 12:36:31 +0200

    Small bufix and refactor of device_info.py file
    - Added "else" switch to return also those results that are not boolean nor digits (14 results including "radio_stack_sram2b", "hardware_model" and many others).
    - Added type hinting.

--- lib/device_info.py
+++ lib/device_info.py
@@ -1,15 +1,23 @@
 import re
 
+from lib.serial_wrapper import SerialWrapper
+
 class DeviceInfo:
-    def __init__(self, serial_wrapper) -> None:
+    def __init__(self, serial_wrapper: SerialWrapper) -> None:
         self._serial_wrapper = serial_wrapper
     
-    def info(self):
+    def info(self) -> dict:
         pattern = re.compile("([\w|_]+)\s+:\s([\w|\d]+)")
         value = {}
-        for result in pattern.findall(self._serial_wrapper.send("device_info")):
-            if result[1].isdigit():
-                value[result[0]] = int(result[1])
-            elif result[1] in ["true", "false"]:
-                value[result[0]] = eval(result[1].title())
+
+        for x in pattern.findall(self._serial_wrapper.send("device_info")):
+            if x[1].isdigit():
+                value[x[0]] = int(x[1])
+
+            elif x[1] in ["true", "false"]:
+                value[x[0]] = eval(x[1].title())
+
+            else:
+                value[x[0]] = x[1]
+
         return value       
