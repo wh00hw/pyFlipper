282eaf5263bce47dd0386c5c9e60db5c811c355c
Till Wenke <till.wenke@gmx.net>
2022-10-17 00:49:44 +0300

    add rfid write, emulate commands

--- lib/rfid.py
+++ lib/rfid.py
@@ -11,3 +11,33 @@ class RFID(Threaded):
             return data
         self.exec(func=None, timeout=timeout)
         return _run()
+
+    def emulate(self, key_type, key_data, timeout: int = 5) -> str:
+        def _run():
+            data = self._serial_wrapper.send("rfid emulate " + key_type + " " + key_data)
+            
+            return data
+        self.exec(func=None, timeout=timeout)
+        return _run()
+
+    # key_type can be one of EM4100, H10301, Indala26, IoProxXSF, AWID, FDX-A, FDX-B, HIDProx, HIDExt, Pyramid, Viking, Jablotron, Paradox, PAC/Stanley, Keri, Gallagher
+    # key_data is just the data you can also read from the lf rfid tags in hex eg '5500824806' from EM4100 type
+
+    def emulate(self, key_type, key_data, timeout: int = 5) -> str:
+        def _run():
+            data = self._serial_wrapper.send("rfid emulate " + key_type + " " + key_data)
+            
+            return data
+        self.exec(func=None, timeout=timeout)
+        return _run()
+
+
+    def write(self, key_type, key_data, timeout: int = 5) -> str:
+        def _run():
+            data = self._serial_wrapper.send("rfid write " + key_type + " " + key_data)
+            
+            return data
+        self.exec(func=None, timeout=timeout)
+        return _run()
+
+    
